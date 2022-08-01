8c5d8b1fe3f8d25d8ec3275b8bd89abd6b9fdda5
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-08-01 08:35:19 -0500

    Setup canvas

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+windows = Tk()
+windows.title("Password Manager")
+windows.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=0, row=0)
+
+
+
+windows.mainloop()
