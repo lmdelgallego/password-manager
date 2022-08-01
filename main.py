534df7ef4bcc55756ab2162e440101131cf3458c
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-08-01 09:06:28 -0500

    👨‍🏭 Ui Setup

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from tkinter import *
+
+# ---------------------------- CONSTANTS ------------------------------- #
+FONT_NAME = "Courier"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -7,13 +10,40 @@ from tkinter import *
 
 windows = Tk()
 windows.title("Password Manager")
-windows.config(padx=20, pady=20)
+windows.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(column=0, row=0)
+canvas.grid(column=1, row=0)
 
+# LABELS
 
+website_label = Label(text="Website:", font=(FONT_NAME, 12))
+website_label.grid(column=0, row=1)
+
+email_username_label = Label(text="Email/Username:", font=(FONT_NAME, 12))
+email_username_label.grid(column=0, row=2)
+
+password_label = Label(text="Password:", font=(FONT_NAME, 12))
+password_label.grid(column=0, row=3)
+
+# ENTRY FIELDS
+website_field = Entry(width=35)
+website_field.grid(column=1, row=1, columnspan=2)
+
+email_username_field = Entry(width=35)
+email_username_field.grid(column=1, row=2, columnspan=2)
+
+password_field = Entry(width=21)
+password_field.grid(column=1, row=3)
+
+# BUTTONS
+
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
 
 windows.mainloop()
