81812c1e9f2f7545c77cd14bb5df8d0e6da197bf
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-08-13 16:05:11 -0500

    👨‍🏭 implement save on json

--- main.py
+++ main.py
@@ -52,13 +52,19 @@ def save():
 
     if len(website) == 0 or len(password) == 0 or len(email) == 0:
         messagebox.showerror(
-            "Oops", "Please make sure you haven'y left any fields empty.")
+            "Oops", "Please make sure you haven't left any fields empty.")
     else:
-        with open("data..json", "r") as data_file:
-            json.dump(new_data, data_file)
-            website_field.delete(0, END)
-            password_field.delete(0, END)
-            print("Password saved.")
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+            print(data)
+            data.update(new_data)
+        with open("data.json", "w") as data_file:
+            json.dump(data, data_file, indent=4)
+
+        website_field.delete(0, END)
+        password_field.delete(0, END)
+
+        print("Password saved.")
 
     # ---------------------------- UI SETUP ------------------------------- #
 
